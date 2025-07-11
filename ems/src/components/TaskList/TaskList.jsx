feea16399ed914d105109efee1db461658f6a61c
AbdulHaseeb87 <abdulhaseebdhedhi115@gmail.com>
2025-07-11 20:12:39 +0500

    Add TaskList component for task rendering

---
+++ ems/src/components/TaskList/TaskList.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import AcceptTask from './AcceptTask'
+import NewTask from './NewTask'
+import CompleteTask from './CompleteTask'
+import FailedTask from './FailedTask'
+
+const TaskList = ({ data }) => {
+  return (
+    <div
+      id="tasklist"
+      className="h-[50%] overflow-x-auto flex items-center justify-start gap-5 flex-nowrap w-full py-1 mt-16 scrollbar-thin scrollbar-thumb-gray-600"
+    >
+      {data.tasks.map((task, idx) => {
+        if (task.active) return <AcceptTask key={idx} data={task} />
+        if (task.newTask) return <NewTask key={idx} data={task} />
+        if (task.completed) return <CompleteTask key={idx} data={task} />
+        if (task.failed) return <FailedTask key={idx} data={task} />
+        return null
+      })}
+    </div>
+  )
+}
+
+export default TaskList
